8eeaf74aa28ce420dabb4d66b957cfc76b2261a7
Tin Zeljar <tinzeljar@gmail.com>
2020-11-17 02:37:09 +0100

    Changes

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -6,4 +6,6 @@ SERVICE1_URL = "http://service1:8080"
 message = sys.stdin.readline()
 data = ["md5", message]
 
-print(requests.post(SERVICE1_URL, data="\n".join(data)).content)
+r = requests.post(SERVICE1_URL, data="\n".join(data))
+
+print(r.text.strip())
