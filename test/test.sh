de5518fce01a4a3f6a59f442f0d4fd18a2bb8759
Tin Zeljar <tinzeljar@gmail.com>
2020-11-17 03:17:41 +0100

    Test 2

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 sleep 5
-if curl -d "test" service2:8080 | grep -q 'test'; then
+if curl -d "test" service2:8080 | grep -q '098f6bcd4621d373cade4e832627b4f6'; then
   echo "Tests passed!"
   exit 0
 else
